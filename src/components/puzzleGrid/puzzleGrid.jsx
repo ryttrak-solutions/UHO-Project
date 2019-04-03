aa556d11afa367d432ac625831e12a755d06a603
ar-naseef <naseef14@gmail.com>
2019-04-03 18:27:26 +0530

    puzzle skeleton is created. Shuffle function added.

--- src/components/puzzleGrid/puzzleGrid.jsx
+++ src/components/puzzleGrid/puzzleGrid.jsx
@@ -3,31 +3,84 @@ import React, { Component } from 'react'
 
 // components
 import Pixel from '../puzzlePixel/puzzlePixel';
+// import PixelWrapper from '../pixelWrapper/pixelWrapper';
 
 export default class puzzleGrid extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
+			gridWidthPx: 800,
+			gridHeightPx: 600,
 			gridWidth: 4,
-			gridHeight: 3
+			gridHeight: 4,
+			image: 'https://magazine.uchicago.edu/views/view1_800x600.jpg',
+			shuffledPixels: null
 		}
 	}
+	
+	componentWillMount() {
+		this.setShuffledPixels();
+	}
+
+	setShuffledPixels = () => {
+		this.setState({
+			shuffledPixels: this.shufflePixels(Array.apply(null, {length: this.state.gridHeight * this.state.gridWidth}).map(Number.call, Number))
+		})
+	}
+
+	shufflePixels = (a) => {
+		for (let i = a.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1));
+				[a[i], a[j]] = [a[j], a[i]];
+		}
+		return a;
+	}
+
+	onClickPixel = (index, positionXY, position) => {
+		const {x,y} = positionXY;
+		console.log(index, position);
+	}
+
 
   render() {
+
 		
+
 		const createGrid = () => {
-			for (let i=0; i<(this.state.gridHeight * this.state.gridWidth); i++) {
-				return(
-					<Pixel i={i} />
+
+			let randomOrder = this.state.shuffledPixels;
+			let pixels = [];
+			for (let i=0; i<(this.state.gridHeight * this.state.gridWidth)-1; i++) {
+				pixels.push(
+					<Pixel
+						key={i}
+						index={i}
+						position={randomOrder[i]}
+						gridWidth={this.state.gridWidth}
+						gridHeight={this.state.gridHeight}
+						gridWidthPx={this.state.gridWidthPx}
+						gridHeightPx={this.state.gridHeightPx}
+						image={this.state.image}
+						onClickPixel={this.onClickPixel}
+					/>
 				)
 			}
+			return pixels;
 		}
 
     return (
       <div>
-        {createGrid()}
-      </div>
+				<button onClick={(e) => {
+					this.setShuffledPixels();
+				}}>Shuffle</button>
+				<div className="puzzleGrid" style={{
+					width: `${this.state.gridWidthPx}px`,
+					height: `${this.state.gridHeightPx}px`
+				}}>
+					{createGrid()}
+				</div>
+			</div>
     )
   }
 }
--- src/components/puzzlePixel/puzzlePixel.jsx
+++ src/components/puzzlePixel/puzzlePixel.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
 
 export default class puzzlePixel extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pixelWidth: this.props.gridWidthPx / this.props.gridWidth,
+      pixelHeight: this.props.gridHeightPx / this.props.gridHeight,
+      positionX: (this.props.index) % this.props.gridWidth,
+      positionY: Math.floor((this.props.index) / this.props.gridWidth)
+    }
+  }
+
   render() {
     return (
-      <div>
-        {this.props.i}
+      <div className="gridPixel" style={{
+        width: `${this.state.pixelWidth}px`,
+        height: `${this.state.pixelHeight}px`,
+        left: `${((this.props.position) % this.props.gridWidth) * this.state.pixelWidth}px`,
+        top: `${(Math.floor((this.props.position) / this.props.gridWidth)) * this.state.pixelHeight}px`,
+        backgroundImage: `url(${this.props.image})`,
+        backgroundSize: `${this.props.gridWidthPx}px ${this.props.gridHeightPx}px`,
+        backgroundPosition: `-${this.state.positionX * this.state.pixelWidth}px -${this.state.positionY * this.state.pixelHeight}px`
+      }} onClick={(e) => {
+        this.props.onClickPixel(this.props.index, {
+          x: this.state.positionX,
+          y: this.state.positionY
+        }, this.props.position)
+      }}>
+        Pixel {this.props.index}
       </div>
     )
   }
